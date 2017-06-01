e987647c0d5af5d883d019d3838eed78aa4ede03
John Westhoff <jwesthof@nd.edu>
2017-06-01 16:59:24 -0400

    'added def and the stack and stuff'

--- crisp/parser.js
+++ crisp/parser.js
@@ -57,7 +57,7 @@ function parse(code) {
             code = code.slice(1);
             return new Node("string", str);
         } else {
-            var id = "";
+            var id = c;
             while (code[0] !== " " && code[0] !== ")" && code[0]) {
                 id += code[0];
                 if (code[0] !== ")") {
@@ -69,7 +69,9 @@ function parse(code) {
     }
     return _parse();
 }
+
 function eval(root) {
+    var stack = [];
     function _eval(n) {
         switch (n.type) {
             case "string":
@@ -82,9 +84,64 @@ function eval(root) {
                 }
                 n.eval = n.children;
                 return n;
+            case "identifier":
+                for (var i = stack.length - 1; i >= 0; i--) {
+                    if (n.value in stack[i]) {
+                        n.eval = stack[i][n.value];
+                        return n;
+                    }
+                }
+                return n;
+            case "function":
+                switch (n.value) {
+                    case "def":
+                        var pairs = _eval(n.children[0]).eval;
+                        for (var i = 1; i < pairs.length; i+=2) {
+                            _eval(pairs[i]);
+                        }
+                        var frame = {};
+                        for (var i = 0; i < pairs.length; i+=2) {
+                            frame[pairs[i].value] = pairs[i+1].eval;
+                        }
+                        stack.push(frame);
+                        n.eval = _eval(n.children[1]);
+                        stack.pop();
+                    break;
+                    case "+":
+                        n.eval = 0;
+                        for (var i = 0; i < n.children.length; i++) {
+                            n.eval += _eval(n.children[i]).eval;
+                        }
+                    break;
+                    case "car":
+                        _eval(n.children[0]);
+                        n.eval = n.children[0].eval[0].eval;
+                    break;
+                    case "cdr":
+                        _eval(n.children[0]);
+                        n.eval = n.children[0].eval.slice(1);
+                    break;
+                    case "cons":
+                        n.eval = new Node("list", "");
+                        n.eval.children =
+                            n.children[n.children.length - 1].children.slice();
+                        for (var i = 0; i < n.children.length - 1; i++) {
+                            n.eval.children.push(n.children[i]);
+                        }
+                        _eval(n.eval);
+                    break;
+                    default:
+                    
+                    break;
+                }
+
+                return n;
         }
     }
     return _eval(root);
 }
 console.log(parse("(die \"123\\\"\" (hi 1.2) 1 -4 '(1 2 3 '(4 5 6)))"));
-console.log(eval(parse("'(\"12\")")));
+console.log(eval(parse("(cdr '((+ 1 2 3) 5 (+ (+ 1 2) 3))")));
+console.log(eval(parse("(cons 1 2 3 '(0))")));
+console.log((parse("(+ x 1)")));
+console.log(eval(parse("(def '(x 1 y 2) (def '(x 4) (+ x y)))")));

05a4df9aa8a73fe58ac3d9d6452598c938fe0011
John Westhoff <johnjwesthoff@gmail.com>
2018-05-31 19:22:27 -0400

    Haha yes

---
+++ jbgames/static/jblib.js
@@ -0,0 +1,38 @@
+var v = 0;
+function client_register(room_code, username, callback) {
+    v = 0;
+    $.get("user_register", {'code': room_code, 'name': username}, function(response) {
+        var data = JSON.parse(response);
+        if (callback) {
+            callback(data);
+        }
+    }).fail(function() {
+        client_register(room_code, username, callback);
+    });
+}
+
+function client_post(room_code, message, callback) {
+    $.get("user_post", {'code': room_code, 'data': message}, function(response) {
+        var data = JSON.parse(response);
+        if (callback) {
+            callback(data);
+        }
+    }).fail(function() {
+        client_post(room_code, message, callback);
+    });
+}
+
+function client_get(room_code, callback) {
+    $.get("user_info", {'code': room_code, 'v': v}, function(response) {
+        var data = JSON.parse(response);
+        if ('v' in data) {
+            v = data['v'];
+        }
+        if (callback) {
+            callback(data);
+        }
+    }).fail(function() {
+        client_get(room_code, callback);
+    });
+}
+
---
+++ jbgames/static/jblib_server.js
@@ -0,0 +1,33 @@
+function server_register(callback) {
+    $.get("new_room", {}, function(response) {
+        var data = JSON.parse(response);
+        if (callback) {
+            callback(data);
+        }
+    }).fail(function() {
+        server_register(callback);
+    });
+}
+
+function server_post(room_code, state, callback) {
+    $.get("server_post", {'code': room_code, 'state': state}, function(response) {
+        var data = JSON.parse(response);
+        if (callback) {
+            callback(data);
+        }
+    }).fail(function() {
+        server_post(room_code, state, callback);
+    });
+}
+
+function server_get(room_code, callback) {
+    $.get("server_info", {'code': room_code}, function(response) {
+        var data = JSON.parse(response);
+        if (callback) {
+            callback(data);
+        }
+    }).fail(function() {
+        server_get(room_code, callback);
+    });
+}
+

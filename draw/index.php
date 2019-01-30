426059ad24fe601ac6b2eeb2f4203d2de363ba1a
John Westhoff <JohnJWesthoff@gmail.com>
2019-01-30 13:45:14 -0500

    Improved

--- draw/index.php
+++ draw/index.php
@@ -7,7 +7,16 @@
 <body onload="loaded()">
     <div align="center">
     <h1>Love Notes</h1>
+    <h2>Happy Birthday, Breezy!</h2>
+    So, my gift to you is this webapp thing. It is an online notesharing
+    app for the two of us. We can draw in the box below, and when we click
+    submit, it will add the drawing to the virtual quilt of all of our
+    love notes to/from each other. You can use your phone, computer, whatever.
+    <br/><br/>
+    I got it started for us.
     <h2>Drawing</h2>
+    You can adjust the size of your utensil, erase, undo, and change color.
+    <br/>
     <canvas id="canvas"></canvas>
     <br>
     <input type="color" id="colorpicker">
@@ -19,6 +28,8 @@
     <button onclick="send();">Submit</button>
     <div id="imagediv"></div>
     <h2>Gallery</h2>
+    Here are all of our shared love notes since those dirty hackers stole my fist batch:
+    <br/>
     <?php
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
@@ -30,8 +41,18 @@
         $cursor = $collection->find($filter, $options);
         foreach ($cursor as $doc)
         {
-            echo "<img src=\"" . $doc["image"] . "\">";
+            $datestr = $doc["date"]->toDateTime()->format("M j, Y");
+            //.toDateTime().format("M j, Y");
+            echo "<img src=\"" . $doc["image"] . "\" title=\"" . $datestr .  "\">";
         }
     ?>
+    <br/>
     </div>
+    <code align='center'>
+&nbsp;_&nbsp;&nbsp;_<br>
+/J\/&nbsp;\<br>
+\&nbsp;+&nbsp;&nbsp;/<br>
+&nbsp;\&nbsp;B/<br>
+&nbsp;&nbsp;\/<br>
+      </code>
 </body>

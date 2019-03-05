149b22da1cb772e3089bd6109b80af4ae9ad9395
John Westhoff <johnjwesthoff@gmail.com>
2019-03-04 23:35:14 -0500

    ok

--- forbrzy/send.sh
+++ forbrzy/send.sh
@@ -2,5 +2,5 @@ rcp=$1
 url=`python2 go.py`
 file=`basename "$url"`
 curl "$url" > $file
-mail -t "$rcp" -A "$file" -s "Hello!" < /dev/null
+mpack "$file" "$rcp" -s "Enjoy a rare pupper!"
 rm "$file"

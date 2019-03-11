69b87284a2f49596cfba0df6e2ff0a89fbe8dfc1
John Westhoff <JohnJWesthoff@gmail.com>
2019-03-11 15:26:03 -0400

    Womp Womp

--- forbrzy/send.sh
+++ forbrzy/send.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+cd /home/john/src/johnwesthoff-blogwork/forbrzy
 rcp=$1
+echo "Sending to $rcp"
 url=`python2 go.py`
+echo "Sending $url"
 file=`basename "$url"`
+echo "Saving to $file"
 curl "$url" > $file
 mpack "$file" "$rcp" -s "Enjoy a rare pupper!"
+mail -s "Here's the link" -t "$rcp" <<EOF
+$url
+EOF
 rm "$file"

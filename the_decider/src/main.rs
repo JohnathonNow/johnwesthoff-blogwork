1b4e18fa692e97ce4a7b4db5de42000d231bc010
John Westhoff <johnjwesthoff@gmail.com>
2021-03-26 16:03:20 -0400

    Adding devil's advocate mode

--- the_decider/src/main.rs
+++ the_decider/src/main.rs
@@ -1,20 +1,25 @@
-use std::env;
+use std::{collections::HashMap, env, sync::Arc};
 use std::error::Error;
 
-use serenity::{
-    async_trait,
-    model::{
-        channel::{Message, ReactionType},
-        gateway::Ready,
-    },
-    prelude::*,
-};
+use serenity::{async_trait, model::{channel::{Message, ReactionType}, gateway::Ready, id::RoleId}, prelude::*};
 
-struct Handler;
+struct Handler {
+    talkers: Arc<Mutex<HashMap<String, i64>>>,
+}
+
+impl Handler {
+    fn new() -> Self {
+        Self {
+            talkers: Arc::new(Mutex::new(HashMap::new())),
+        }
+    }
+}
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
+        let mut talkers = self.talkers.lock().await;
+        talkers.insert(msg.author.name.clone(), msg.timestamp.timestamp());
         
         let v = match &msg.content {
             x if x.starts_with("??") => vec!["⏫", "🔼", "⏸", "🔽", "⏬"],
@@ -28,6 +33,12 @@ impl EventHandler for Handler {
                 .await
                 .ok();
         }
+        if let Some(x) = msg.guild_id {
+            if let Ok(mut y) = x.member(&ctx, msg.author.id).await {
+                y.add_role(&ctx, RoleId(825089774251147355)).await.unwrap_or(());
+            }
+        }
+        println!("{:?}", talkers);
     }
 
     async fn ready(&self, _: Context, ready: Ready) {
@@ -38,7 +49,7 @@ impl EventHandler for Handler {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let token = env::var("DISCORD_TOKEN")?;
-    let mut client = Client::builder(&token).event_handler(Handler).await?;
+    let mut client = Client::builder(&token).event_handler(Handler::new()).await?;
     client.start().await?;
     Ok(())
 }

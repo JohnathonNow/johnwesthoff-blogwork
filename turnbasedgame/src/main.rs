867ee7e42b069c2f1198051f33ea762a25a72bdd
John Westhoff <johnjwesthoff@gmail.com>
2021-04-22 00:10:13 -0400

    Improved code a little

--- turnbasedgame/src/main.rs
+++ turnbasedgame/src/main.rs
@@ -15,28 +15,27 @@ async fn index(
     web::Path((id, name)): web::Path<(u32, String)>,
     conn: web::Data<Arc<Mutex<Connection>>>,
 ) -> impl Responder {
-    conn.lock()
-        .unwrap()
-        .execute(
-            "INSERT INTO posts (num, user) VALUES (?1, ?2)",
-            params![id, name],
-        )
-        .unwrap();
-
-    format!("Hello {}! id:{}", name, id)
+    if let Ok(_) = conn.lock().unwrap().execute(
+        "INSERT INTO posts (num, user) VALUES (?1, ?2)",
+        params![id, name],
+    ) {
+        format!("Hello {}! id:{}", name, id)
+    } else {
+        format!("big sad!")
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     if let Ok(conn) = Connection::open(&"./bob.db") {
-        conn.execute(
+        let _ = conn.execute(
             "CREATE TABLE posts (
                   id              INTEGER PRIMARY KEY,
                   num             INT8,
                   user            INT8
                   )",
             params![],
-        ).unwrap();
+        );
         let x = web::Data::new(Arc::new(Mutex::new(conn)));
         HttpServer::new(move || App::new().service(index).app_data(x.clone()))
             .bind("127.0.0.1:8080")?

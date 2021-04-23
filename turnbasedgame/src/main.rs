ab3a52b2d16c8e42db0d9c2805ce4976bbd2f796
John Westhoff <johnjwesthoff@gmail.com>
2021-04-22 23:49:46 -0400

    Improved program

--- turnbasedgame/src/main.rs
+++ turnbasedgame/src/main.rs
@@ -1,28 +1,68 @@
 use std::sync::Arc;
 use std::sync::Mutex;
 
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, http::StatusCode, web, App, HttpResponse, HttpServer, ResponseError};
 use rusqlite::{params, Connection};
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize)]
 struct Post {
     id: i64,
     num: i64,
     user: String,
 }
+
+#[derive(Debug)]
+struct Error {}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("bob")
+    }
+}
+
+impl From<rusqlite::Error> for Error {
+    fn from(_: rusqlite::Error) -> Self {
+        Self {}
+    }
+}
+
+impl ResponseError for Error {
+    fn status_code(&self) -> StatusCode {
+        StatusCode::INTERNAL_SERVER_ERROR
+    }
+
+    fn error_response(&self) -> HttpResponse {
+        "so sad".into()
+    }
+}
+
+#[get("/all/")]
+async fn index2(conn: web::Data<Arc<Mutex<Connection>>>) -> Result<HttpResponse, Error> {
+    let l = conn.lock().unwrap();
+    let mut stmt = l.prepare("SELECT id, num, user FROM posts")?;
+    let iter = stmt
+        .query_map(params![], |row| {
+            Ok(Post {
+                id: row.get(0)?,
+                num: row.get(1)?,
+                user: row.get(2)?,
+            })
+        })?
+        .filter(|a| a.is_ok())
+        .map(|x| x.unwrap());
+    Ok(HttpResponse::Ok().json(iter.collect::<Vec<Post>>()))
+}
 #[get("/{id}/{name}")]
 async fn index(
     web::Path((id, name)): web::Path<(u32, String)>,
     conn: web::Data<Arc<Mutex<Connection>>>,
-) -> impl Responder {
-    if let Ok(_) = conn.lock().unwrap().execute(
+) -> Result<HttpResponse, Error> {
+    let r = conn.lock().unwrap().execute(
         "INSERT INTO posts (num, user) VALUES (?1, ?2)",
         params![id, name],
-    ) {
-        format!("Hello {}! id:{}", name, id)
-    } else {
-        format!("big sad!")
-    }
+    )?;
+    Ok(HttpResponse::Ok().json(r))
 }
 
 #[actix_web::main]
@@ -37,10 +77,15 @@ async fn main() -> std::io::Result<()> {
             params![],
         );
         let x = web::Data::new(Arc::new(Mutex::new(conn)));
-        HttpServer::new(move || App::new().service(index).app_data(x.clone()))
-            .bind("127.0.0.1:8080")?
-            .run()
-            .await?;
+        HttpServer::new(move || {
+            App::new()
+                .service(index2)
+                .service(index)
+                .app_data(x.clone())
+        })
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await?;
     }
     Ok(())
 }

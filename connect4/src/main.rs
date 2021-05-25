e60ce526c275ba1f89f459e47a5afaecc54ecc2b
John Westhoff <jwesthof@nd.edu>
2021-05-25 14:15:22 -0400

    small change

--- connect4/src/main.rs
+++ connect4/src/main.rs
@@ -25,6 +25,9 @@ fn get_move(b: &Board, piece: i8) -> usize {
     let mut possible_futures = [b; 7];
     for i in 0..7 {
         possible_futures[i].place(piece, i);
+        if possible_futures[i].is_won() == piece {
+            return i;
+        }
     }
     // search depth 1 for any wins, go with those if we can
     // else choose two random moves, a and b, and recurse on them

e21f09c528a37a5430ba1f8e38c116f77a7ea138
John Westhoff <johnjwesthoff@gmail.com>
2021-05-24 23:32:12 -0400

    Add readme

---
+++ connect4/Readme.md
@@ -0,0 +1,6 @@
+Connect4
+========
+
+A set of bots for playing Connect4, mostly to explore
+the idea of fully analyzing boards assuming each player
+can only play a random small subset of moves.
--- connect4/src/main.rs
+++ connect4/src/main.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Board {
     board: [[i8; 6] ; 7],
 }
@@ -6,8 +6,22 @@ impl Board {
     fn new() -> Self {
         Board { board: [[0;6];7] }
     }
+
+    fn place(&mut self, piece: i8, row: usize) {
+        for i in 0..6 {
+            if self.board[row][i] == 0 {
+                self.board[row][i] = piece;
+                return;
+            }
+        }
+    }
+   
+    fn is_won(&self) -> i8 {
+        0     
+    }
 }
 fn main() {
-    let b = Board::new();
+    let mut b = Board::new();
+    b.place(1, 3);
     println!("{:?}", b);
 }
